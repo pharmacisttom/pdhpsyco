9b9c0983fa5b001ca1f3ca66871bdbd8a7b4d17e
pharmacisttom <pharmacisttom@users.noreply.github.com>
2026-07-05 12:40:03 +0700

    Update Apps Script URL

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // Replace this URL with the Google Apps Script Web App URL you get after deploying.
-export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxGyiEpwekQIDTf0wlr6rMAo6616NbWR9iVaoBA0OM538r2mnWQSfn7O7YU8dutQQgH8g/exec";
+export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx9LVu4CWY5yXIuIQcdEznzR3jzelmCEM2VTWSF01jsCX48wdUItlcq_S4-rTlkStLXgQ/exec";
